f5f163c2a553c9a30d0b2029e0cf0b2ea40c771d
Adán Omar Quevedo Argueta <adanque12dst@gmail.com>
2025-10-13 09:20:08 -0600

    4 bases nitrogenadas

---
+++ SDS25/app/views/HomeView.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <H1>EL DIAVLO LOCO</H1>
+    <h4>kb2</h4>
+</body>
+</html>
--- SDS25/routes/web.php
+++ SDS25/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 use lib\Route;
+use app\controllers\HomeController;
 
-Route::get("/", function(){
-    echo "RUTA RAIZ";
-});
+//Route::get("/", function(){ echo "RUTA RAIZ"; });
+
+Route::get("/", [HomeController::class, 'index']);
 
 Route::get("/inicio/:flag", function($flag){
     return ["title"=>"ejemplo"];
