6da571b49381770b9ce381b84bc53dc937160f90
Adán Omar Quevedo Argueta <adanque12dst@gmail.com>
2025-10-14 17:57:41 -0600

    estilos basicos

--- SDS25/app/controllers/HomeController.php
+++ SDS25/app/controllers/HomeController.php
@@ -21,5 +21,8 @@ class HomeController extends controller{
         return $this->view('HomeRegistro');
     }
     
+    public function registrar(){
+        
+    }
 }
 ?>
--- SDS25/routes/web.php
+++ SDS25/routes/web.php
@@ -4,11 +4,13 @@ use app\controllers\HomeController;
 
 //Route::get("/", function(){ echo "RUTA RAIZ"; });
 
-Route::get("/", [HomeController::class, 'index']);
+Route::get("/inicio", [HomeController::class, 'index']);
 
 Route::get("/visitantes", [HomeController::class, 'visitantes']);
 
 Route::get("/registro", [HomeController::class, 'registro']);
+
+Route::post("/registrar");
 
 Route::dispatch();
 ?>
